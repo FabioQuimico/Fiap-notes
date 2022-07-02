46940846fb688fb480b712eef31b96de747a2bba
FabioQuimico <fabioquimico@gmail.com>
2022-07-02 09:57:47 -0300

    note configurada

--- src/app/views/pages/form-note/form-note.component.ts
+++ src/app/views/pages/form-note/form-note.component.ts
@@ -1,11 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-form-note',
+  selector: 'app-form-note', //Nome para referenciar o componente
   templateUrl: './form-note.component.html',
   styleUrls: ['./form-note.component.css']
 })
 export class FormNoteComponent implements OnInit {
+  titulo= "FIAP NOTES";
+  imagemLogo = "assets/logo.png";
 
   constructor() { }
 
--- src/app/views/pages/note/note.component.ts
+++ src/app/views/pages/note/note.component.ts
@@ -7,9 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NoteComponent implements OnInit {
 
+  note = {
+    id: 1,
+    data: new Date(),
+    text: 'Um texto qualquer',
+    urgent: true,
+  }
+  
   constructor() { }
 
+
   ngOnInit(): void {
+  }
+
+  removeNote() {
+    alert("Remover nota");
   }
 
 }
